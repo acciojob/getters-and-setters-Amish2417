3032020a33d0690692bcd2391e071985a85f3417
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-05-15 17:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,35 +25,6 @@ window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
 
-You  to  Ayush Acciojob (direct message) 16:57
+//You  to  Ayush Acciojob (direct message) 16:57
 //complete this code
-class Person {
-	constructor(name, age) {
-		this.newName = name;
-        this.newAge = age;	
-    }
-	get name() {
-    return this.newName;
-     }
-	set age(age) {
-		this.newAge = age;
-     }
-	
-}
 
-class Student extends Person {
-	study(){
-		console.log(this.newName + " is studying");
-	}
-}
-
-class Teacher extends Person {
-	teach(){
-		console.log(this.newName + " is teaching");
-	}
-}
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
